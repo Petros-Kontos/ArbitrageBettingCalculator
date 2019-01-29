40bd8b8fbe2d577d1f0926d930fa0435e02db68d
Petros Kontos <p_kontos@hotmail.com>
2019-01-29 10:49:12 +0000

    Program.cs

--- ArbitrageBettingCalculator/Program.cs
+++ ArbitrageBettingCalculator/Program.cs
@@ -8,38 +8,101 @@ class ArbitrageBettingCalculator
 {
     
     static void Main(string[] args)
-    {
-
-        //Initial variable declarations
-        
-        int payoutWin, payoutLoss, payoutDraw, betAmountWin, betAmountLoss, betAmountDraw, grossProfitWin, grossProfitLoss, grossProfitDraw, netProfitWin, netProfitLoss, netProfitDraw;
+    {   
+        int betAmountWin, betAmountLoss, betAmountDraw;
+        float payoutWin, payoutLoss, payoutDraw, grossProfitWin, grossProfitLoss, grossProfitDraw, netProfitWin, netProfitLoss, netProfitDraw;
         string payoutWinString, payoutLossString, payoutDrawString, betAmountWinString, betAmountLossString, betAmountDrawString, restart;
+
+        const int DEFAULT_VALUE = 0;
+
+        payoutWin = payoutLoss = payoutDraw = betAmountWin = betAmountLoss = betAmountDraw = DEFAULT_VALUE;
+
 
         //Getting the inputs from the user
 
-        Console.WriteLine("Please enter the payout factor in case of a win");
-        payoutWinString = Console.ReadLine();
-        payoutWin = int.Parse(payoutWinString);
+        Console.WriteLine("\n");
+        Console.WriteLine("Please enter the payout factor in case of a win:");
+        Console.WriteLine("\n");
+        try
+        {
+            payoutWinString = Console.ReadLine();//this var is not used outside of this block. (That's why it doesn't need to be initialised outside the block.) Should I declare it inside the block??
+            payoutWin = float.Parse(payoutWinString);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(e.Message + " A default value of " + DEFAULT_VALUE + " has been used.");
+        }
 
+        Console.WriteLine("\n");
         Console.WriteLine("Please enter the payout factor in case of a loss");
-        payoutLossString = Console.ReadLine();
-        payoutLoss = int.Parse(payoutLossString);
+        Console.WriteLine("\n");
+        try
+        {
+            payoutLossString = Console.ReadLine();
+            payoutLoss = float.Parse(payoutLossString);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(e.Message + " A default value of " + DEFAULT_VALUE + " has been used.");
+        }
 
+        Console.WriteLine("\n");
         Console.WriteLine("Please enter the payout factor in case of a draw");
-        payoutDrawString = Console.ReadLine();
-        payoutDraw = int.Parse(payoutDrawString);
+        Console.WriteLine("\n");
+        try
+        {
+            payoutDrawString = Console.ReadLine();
+            payoutDraw = float.Parse(payoutDrawString);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(e.Message + " A default value of " + DEFAULT_VALUE + " has been used.");
+        }
 
-        Console.WriteLine("Please enter the amount that you wish to bet on a win (in pennies)");
-        betAmountWinString = Console.ReadLine();
-        betAmountWin = int.Parse(betAmountWinString);
+        Console.WriteLine("\n");
+        Console.WriteLine("Please enter the amount that you wish to bet on a win (in pence)");
+        Console.WriteLine("\n");
+        try
+        {
+            betAmountWinString = Console.ReadLine();
+            betAmountWin = int.Parse(betAmountWinString);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(e.Message + " A default value of " + DEFAULT_VALUE + " has been used.");
+        }
 
-        Console.WriteLine("Please enter the amount that you wish to bet on a loss (in pennies)");
-        betAmountLossString = Console.ReadLine();
-        betAmountLoss = int.Parse(betAmountLossString);
+        Console.WriteLine("\n");
+        Console.WriteLine("Please enter the amount that you wish to bet on a loss (in pence)");
+        Console.WriteLine("\n");
+        try
+        {
+            betAmountLossString = Console.ReadLine();
+            betAmountLoss = int.Parse(betAmountLossString);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(e.Message + " A default value of " + DEFAULT_VALUE + " has been used.");
+        }
 
-        Console.WriteLine("Please enter the amount that you wish to bet on a draw (in pennies)");
-        betAmountDrawString = Console.ReadLine();
-        betAmountDraw = int.Parse(betAmountDrawString);
+        Console.WriteLine("\n");
+        Console.WriteLine("Please enter the amount that you wish to bet on a draw (in pence)");
+        Console.WriteLine("\n");
+        try
+        {
+            betAmountDrawString = Console.ReadLine();
+            betAmountDraw = int.Parse(betAmountDrawString);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(e.Message + " A default value of " + DEFAULT_VALUE + " has been used.");
+        }
 
         //Calc standalone method...
 
@@ -47,9 +110,9 @@ class ArbitrageBettingCalculator
 
         //Calculation and output
 
-        grossProfitWin = (payoutWin) * betAmountWin;
-        grossProfitLoss = (payoutLoss) * betAmountLoss;
-        grossProfitDraw = (payoutDraw) * betAmountDraw;
+        grossProfitWin = payoutWin * betAmountWin;
+        grossProfitLoss = payoutLoss * betAmountLoss;
+        grossProfitDraw = payoutDraw * betAmountDraw;
 
         netProfitWin = grossProfitWin - betAmountLoss;
         netProfitLoss = grossProfitLoss - betAmountWin;
@@ -60,10 +123,10 @@ class ArbitrageBettingCalculator
             Console.WriteLine("\n");
             Console.WriteLine("Profit is guarranteed!");
         }
-        else if (netProfitWin < 0 & netProfitLoss < 0 & netProfitDraw > 0)
+        else if (netProfitWin <= 0 & netProfitLoss <= 0 & netProfitDraw <= 0)
         {
             Console.WriteLine("\n");
-            Console.WriteLine("Loss is certain...");
+            Console.WriteLine("No profit can be made...");
         }
         else
         {
